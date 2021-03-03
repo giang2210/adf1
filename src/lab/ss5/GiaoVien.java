cb56d14961b0dccb91901e382763f2f3d21d1b59
giang <70989443+giang2210@users.noreply.github.com>
2021-03-03 08:46:24 +0700

    kk

--- src/lab/ss5/GiaoVien.java
+++ src/lab/ss5/GiaoVien.java
@@ -1,62 +1,74 @@
 package lab.ss5;
 
+import lab.s4_2.LopHoc;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class GiaoVien extends Person {
-    String mucluong;
-    String somongiangday;
-    String cacmongiangday;
-    ArrayList<Person> danhsachlopgiangday;
+    double mucLuong;
+    int soMonGiangDay;
+    ArrayList <String>cacMonGiangDay;
+    ArrayList<String> danhSachLopGiangDay;
 
     public GiaoVien() {
-        danhsachlopgiangday = new ArrayList<Person>();
+        danhSachLopGiangDay = new ArrayList<String>();
+        cacMonGiangDay = new ArrayList<String>();
+    }
+    public double getMucLuong() {
+        return mucLuong;
     }
 
-    public String getMucluong() {
-        return mucluong;
+    public void setMucLuong(double mucLuong) {
+        this.mucLuong = mucLuong;
     }
 
-    public void setMucluong(String mucluong) {
-        this.mucluong = mucluong;
+    public int getSoMonGiangDay() {
+        return soMonGiangDay;
     }
 
-    public String getSomongiangday() {
-        return somongiangday;
+    public void setSoMonGiangDay(int soMonGiangDay) {
+        this.soMonGiangDay = soMonGiangDay;
     }
 
-    public void setSomongiangday(String somongiangday) {
-        this.somongiangday = somongiangday;
-    }
-
-    public String getCacmongiangday() {
-        return cacmongiangday;
-    }
-
-    public void setCacmongiangday(String cacmongiangday) {
-        this.cacmongiangday = cacmongiangday;
+    {
     }
 
 
-    public void inputPerson() {
+    public void nhapGiaoVien() {
         super.inputtt();
         Scanner p = new Scanner(System.in);
         System.out.println("muc luong: ");
-        setMucluong(p.nextLine());
+        setMucLuong(p.nextDouble());
         System.out.println("so mon giang day: ");
-        setSomongiangday(p.nextLine());
-        System.out.println("cac mon giang day: ");
-        setCacmongiangday(p.nextLine());
+        setSoMonGiangDay(p.nextInt());
+        p.nextLine();
+        System.out.println("Danh sach cac mon: ");
+        for (int i = 0; i < getSoMonGiangDay(); i++) {
+         danhSachLopGiangDay.add(p.nextLine());
+        }
+        System.out.println("so lop day: ");
+        int sl = p.nextInt();
+        p.nextLine();
+        System.out.println("danh sách lớp: ");
+        for (int i = 0; i < sl; i++) {
+            danhSachLopGiangDay.add(p.nextLine());
+        }
     }
-    public void addDanhSach(Person ds){
-        danhsachlopgiangday.add(ds);
-    }
-
-    public void printPerson() {
+    public void showInfo(){
         super.printtt();
-        System.out.println("muc luong: " + getMucluong());
-        System.out.println("so mon giang day: " + getSomongiangday());
-        System.out.println("cac mon giang day: " + getCacmongiangday());
+        System.out.println("muc luong: "+getMucLuong());
+        System.out.println("so mon hoc: "+getSoMonGiangDay());
+        System.out.println("danh sach mon hoc: ");
+        for (String s : cacMonGiangDay) {
+            System.out.println(s);
+        }
+        System.out.println("danh sach lop hoc: ");
+        for (String s: danhSachLopGiangDay){
+            System.out.println(s);
+        }
     }
 
-}
+
+    }
+
--- src/lab/ss5/Main.java
+++ src/lab/ss5/Main.java
@@ -1,51 +1,13 @@
 package lab.ss5;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import lab.s4_2.LopHoc;
+import lab.s4_2.SinhVien;
 
 public class Main {
     public static void main(String[] args) {
-       /*Person t = new Person();
-        t.inputtt();
-        t.printtt();
-*/
-
-           /* Student t2 = new Student();
-            t2.inputPerson();
-            t2.printtt();
-            t2.kiemtra();*/
-        ArrayList<GiaoVien> danhsachlopgiangday = new ArrayList<>();
-        Scanner p = new Scanner(System.in);
-        for (int i = 0; i < 2; i++) {
-            GiaoVien t4 = new GiaoVien();
-            System.out.println("ten: ");
-            t4.setTen(p.nextLine());
-            System.out.println("gioi tinh: ");
-            t4.setGioitinh(p.nextLine());
-            System.out.println("ngay sinh: ");
-            t4.setNgaysinh(p.nextLine());
-            System.out.println("dia chi: ");
-            t4.setDiachi(p.nextLine());
-            System.out.println("muc luong: ");
-            t4.setMucluong(p.nextLine());
-            System.out.println("so mon giang day: ");
-            t4.setSomongiangday(p.nextLine());
-            System.out.println("cac mon giang day: ");
-            t4.setCacmongiangday(p.nextLine());
-
-
-            danhsachlopgiangday.add(t4);
-
-        }
-        p.close();
-        System.out.println("danh sach lop giang day: ");
-        for (GiaoVien giaovien : danhsachlopgiangday) {
-            System.out.println(giaovien.toString());
-        }
-
-
-
-
+        // Khoi tao lop hoc
+        GiaoVien gv = new GiaoVien();
+        gv.nhapGiaoVien();
+        gv.showInfo();
     }
-
 }
